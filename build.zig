cb475e52c12abf2a0c78f11beef20264a93864a4
johnphillips114 <johnphillips114@gmail.com>
2026-07-05 18:20:36 -0600

    add raylib dependency

--- build.zig
+++ build.zig
@@ -11,6 +11,15 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
     const exe = b.addExecutable(.{
         .name = "keyboard",
         .root_module = b.createModule(.{
@@ -23,6 +32,10 @@ pub fn build(b: *std.Build) void {
             },
         }),
     });
+
+    exe.root_module.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     exe.root_module.addObjectFile(b.path("libs/portaudio/libportaudio.a"));
     exe.root_module.linkSystemLibrary("asound", .{});
